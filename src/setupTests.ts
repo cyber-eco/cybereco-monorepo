9a06731def2ea31e55e2ecdeea909bbd61ceaee0
Artemio Padilla <artemiopadilla@gmail.com>
2025-05-19 22:07:12 -0600

    chore: update testing setup with Jest dependencies; refactor test files for App and Logo components

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,11 @@
 // Jest setup file
 // This file will be automatically picked up by Jest
 // We're keeping it minimal for now
+
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+// This file is automatically included by Create React App when running tests
+
+// If you need to mock modules, you can do it here
